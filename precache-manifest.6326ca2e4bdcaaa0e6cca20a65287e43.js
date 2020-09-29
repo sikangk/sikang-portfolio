8085ff8affa6a3b092683c9f1e79c35c0b5dc09f
sikang <updream4@naver.com>
2020-09-29 12:40:48 +0900

    Updates

---
+++ precache-manifest.6326ca2e4bdcaaa0e6cca20a65287e43.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b094a7b092ba69db6b8e8987002060",
+    "url": "/sikang-portfolio/index.html"
+  },
+  {
+    "revision": "99231d2e651a08647558",
+    "url": "/sikang-portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "dbc5531c9431b4b90071",
+    "url": "/sikang-portfolio/static/js/2.336a8fb9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sikang-portfolio/static/js/2.336a8fb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99231d2e651a08647558",
+    "url": "/sikang-portfolio/static/js/main.d6f877fe.chunk.js"
+  },
+  {
+    "revision": "3b72b3499c560d824ccb",
+    "url": "/sikang-portfolio/static/js/runtime-main.2459b880.js"
+  },
+  {
+    "revision": "117a30f0866daefba994715b3b86d157",
+    "url": "/sikang-portfolio/static/media/dreamer.117a30f0.jpg"
+  },
+  {
+    "revision": "fb1e7f9db2540c3194a9179094a925e2",
+    "url": "/sikang-portfolio/static/media/html.fb1e7f9d.png"
+  },
+  {
+    "revision": "fd2220dbb5a9c400d06b771a0a6b01ed",
+    "url": "/sikang-portfolio/static/media/linux.fd2220db.png"
+  },
+  {
+    "revision": "bab2c760c60f17191cb3a002e08a3dbf",
+    "url": "/sikang-portfolio/static/media/mysql.bab2c760.png"
+  },
+  {
+    "revision": "c214ae89c547ebd9ce34d0642fbb381b",
+    "url": "/sikang-portfolio/static/media/node.c214ae89.png"
+  },
+  {
+    "revision": "110de0e6f19d01f30a47f7a11542de51",
+    "url": "/sikang-portfolio/static/media/oracle.110de0e6.png"
+  },
+  {
+    "revision": "22eeecf99b8d92726b4f3735be5473c0",
+    "url": "/sikang-portfolio/static/media/python.22eeecf9.png"
+  },
+  {
+    "revision": "70f87bba34487dc1c0cb8916d800c63b",
+    "url": "/sikang-portfolio/static/media/react.70f87bba.png"
+  },
+  {
+    "revision": "6597cc35589236a430e20f4d2adf09a6",
+    "url": "/sikang-portfolio/static/media/sass.6597cc35.png"
+  }
+]);

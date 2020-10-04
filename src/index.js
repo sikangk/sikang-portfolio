46f1da9eb3c40a827bfadcc4c62c1ae612f97339
sikang <updream4@naver.com>
2020-10-04 20:25:18 +0900

    ie version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
 import App from "./App";
 
